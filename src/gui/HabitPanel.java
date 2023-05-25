435a11537918a34c41723434e44c3ed7571e8ce3
Ivan Sy <ivansyrups@gmail.com>
2023-05-25 08:46:21 -0700

    HabitPanel work

--- src/gui/HabitPanel.java
+++ src/gui/HabitPanel.java
@@ -1,10 +1,14 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class HabitPanel extends JPanel {
 
     public HabitPanel() {
+
         this.setBorder(BorderFactory.createTitledBorder("Habits"));
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
     }
 }
